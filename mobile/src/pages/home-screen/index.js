f5b59217fabb928aea6297715be311bdf091bb98
Bruno Santoni <bruno.santoni@fatecitapetininga.edu.br>
2020-04-13 03:51:28 -0300

    feat: backHandler funfando
    Co-authored-by: miguelaugl <miguelaugl@outlook.com>

--- mobile/src/pages/home-screen/index.js
+++ mobile/src/pages/home-screen/index.js
@@ -17,28 +17,24 @@ export default function HomeScreen(props){
     const [id, setId] = useState('')
     const [picture, setPicture] = useState('')
     const [loading, setLoading] = useState(true)
-    const [next, setNext] = useState(1)
-
+    // const [next, setNext] = useState(1)
     const navigation = useNavigation()
 
-    const [content, setContent] = useState(
-        <Screen>
-            <View style={styles.stewardContainer}>
-                <Image source={loading ? Loading : {uri: picture}} style={styles.img} />
-                <Txt style={{fontSize: 20}}>Olá, {username}</Txt>
-                <Txt style={{fontSize: 20}}>O que posso fazer por você hoje?</Txt>
-            </View>
-
-            <View style={styles.optionContainer}>
-                <TouchButton style={styles.firstOption} textStyle={styles.optionText}>Encontre a melhor oferta</TouchButton>
-                <TouchButton style={styles.secondOption} textStyle={styles.optionText}>Encontre um comércio ou produto próximo</TouchButton>
-            </View>
-        </Screen>
-    )
-    
+    const backAction = () => {
+        Alert.alert("Aviso", "Tem certeza que quer sair?", [
+          {
+            text: "Não",
+            onPress: () => null,
+            style: 'default'
+          },
+          { text: "Sim", onPress: () => navigation.goBack() }
+        ]);
+        return true;
+      };
 
     //BOTA TUDO DENTRO DO ASYNC SENÃO NÃO ESPERA
     useEffect(() => {
+        BackHandler.addEventListener("hardwareBackPress", backAction);
         async function getUserEmail() {
             const user = await AsyncStorage.getItem('user_mail')
             const user_id = await AsyncStorage.getItem('user_id')
@@ -55,87 +51,27 @@ export default function HomeScreen(props){
             })
         }
         getUserEmail()
+        return () =>
+        BackHandler.removeEventListener("hardwareBackPress", backAction);
     }, [id])
 
     function navigateToProductList(){
         navigation.navigate('ProductList')
         setNext(0)
     }
+    
+    return (
+        <Screen>
+            <View style={styles.stewardContainer}>
+                <Image source={loading ? Loading : {uri: picture}} style={styles.img} />
+                <Txt style={{fontSize: 20}}>Olá, {username}</Txt>
+                <Txt style={{fontSize: 20}}>O que posso fazer por você hoje?</Txt>
+            </View>
 
-    useBackHandler(() => {
-        if(next === 2){
-            setContent(
-            <Screen>
-                <View style={styles.stewardContainer}>
-                    <Image source={loading ? Loading : {uri: picture}} style={styles.img} />
-                    <Txt style={{fontSize: 20}}>Olá, {username}</Txt>
-                    <Txt style={{fontSize: 20}}>O que posso fazer por você hoje?</Txt>
-                </View>
-
-                <View style={styles.optionContainer}>
-                    <TouchButton style={styles.firstOption} textStyle={styles.optionText}>Encontre a melhor oferta</TouchButton>
-                    <TouchButton style={styles.secondOption} textStyle={styles.optionText}>Encontre um comércio ou produto próximo</TouchButton>
-                </View>
-            </Screen>
-            )
-
-            setNext(1)
-            return true
-        } else if(next === 1){
-            Alert.alert('SAIR', 'Tem certeza que quer sair?',[
-            {   
-                text: 'sim', 
-                onPress: () => {
-                    BackHandler.exitApp()
-                },
-                style: 'default'
-            },
-
-            {
-                text: 'não',
-                onPress: () => {},
-                style: 'default'
-            }
-            ])
-        return true
-        }
-        // setNext(2)
-        // return false
-    })
-
-    function optOneHandler(){
-        setContent(
-            <Screen>
-                <View style={styles.stewardContainer}>
-                    <Image source={loading ? Loading : {uri: picture}} style={styles.img} />
-                    <Txt style={{fontSize: 20}}>Certo,</Txt>
-                    <Txt style={{fontSize: 20}}>me informe o nome do produto:</Txt>
-                </View>
-
-                <View style={styles.inputContainer}>
-                    <TxtInput placeholder="> Insira aqui..." style={{ height: 50 }} />
-                    <TxtInput placeholder="> Insira aqui..." style={{ height: 50 }} onSubmitEditing={() => navigateToProductList()} />
-                </View>
-            </Screen>
-        )
-        setNext(2)
-    }
-
-    function optTwoHandler() {
-        setContent(
-            <Screen>
-                <View style={styles.stewardContainer}>
-                    <Image source={loading ? Loading : {uri: picture}} style={styles.img} />
-                    <Txt style={{fontSize: 20}}>Certo,</Txt>
-                    <Txt style={{fontSize: 20}}>me informe o nome do supermercado:</Txt>
-                </View>
-                <View style={styles.inputContainer2}>
-                    <TxtInput placeholder="> Insira aqui..." style={{ height: 50 }} />
-                </View>
-            </Screen>
-        )
-        setNext(2)
-    }
-
-    return content
+            <View style={styles.optionContainer}>
+                <TouchButton style={styles.firstOption} textStyle={styles.optionText}>Encontre a melhor oferta</TouchButton>
+                <TouchButton style={styles.secondOption} textStyle={styles.optionText}>Encontre um comércio ou produto próximo</TouchButton>
+            </View>
+        </Screen>
+    )
 }   
