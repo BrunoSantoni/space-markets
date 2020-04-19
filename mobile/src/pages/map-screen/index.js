18eb317175484e810ca27e1f22d901853fd45a0e
pedrov4z <61953061+pedrov4z@users.noreply.github.com>
2020-04-19 17:41:52 -0300

    feat: buscar antes de renderizar

--- mobile/src/pages/map-screen/index.js
+++ mobile/src/pages/map-screen/index.js
@@ -1,6 +1,6 @@
 // pedrov4z
 
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import {
     Text,
     Image,
@@ -24,104 +24,114 @@ export default function MapScreen() {
     const mapRef = useRef(null)
     const navigation = useNavigation()
     const [mercados, setMercados] = useState([])
+    const [isBusy, setBusy] = useState(true)
     
-    const loadMercados = () => {
-        api.get('mercados')
-        .then(res => setMercados(res.data))
-    }
+    useEffect(() => {
+        api.get('mercados').then(res => {
+            setMercados(res.data)
+            console.log(res.data)
+            setBusy(false)
+        })
+    }, [])
+    
 
     function navigateToSuggest() {
         navigation.navigate('Suggest')
     }
 
+    function showFirstCallout() {
+        mercados[0].markRef.showCallout()
+    }
+
     return(
-        <View style={styles.container}>
-            <Logo />
-            <MapView
-            ref={mapRef}
-            initialRegion={{
-                latitude: -23.57881,
-                longitude: -48.03744,
-                latitudeDelta: 0.0142,
-                longitudeDelta: 0.0131,
-            }}
-            style={styles.mapView}
-            rotateEnabled={false}
-            scrollEnabled={true}
-            zoomEnabled={false}
-            showsPointsOfInterest={false}
-            showsBuildings={false}
-            showsUserLocation={true}
-            showsMyLocationButton={true}
-            onMapReady={loadMercados}
-            >
-                {mercados.map((mercado) => (
-                    <MapView.Marker
-                    ref={(markRef) => (mercado.markRef = markRef)}
-                    title={mercado.market_name}
-                    description={mercado.market_street + ', ' + mercado.market_number}
-                    key={mercado.market_id}
-                    coordinate={{
-                        latitude: mercado.market_latitude,
-                        longitude: mercado.market_longitude,
-                    }}
-                    >
-                        <Image
-                        source={require('../../../assets/img/marker.png')}
-                        style={styles.marker}
-                        />
-                    </MapView.Marker>
-                ))}
-            </MapView>
-            
-            <ScrollView
-            style={styles.placesContainer}
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            pagingEnabled
-            onMomentumScrollEnd={(e) => {
-                const scrolled = e.nativeEvent.contentOffset.x
-
-                const place = scrolled > 0 ? scrolled / width : 0
-            
-                console.log('prevPlace: ' + prevPlace)
-                console.log('place: ' + place)
-
-                if (place != prevPlace) {
-                    const { market_latitude, market_longitude, markRef } = mercados[place]
-
-                    mapRef.current.setCamera(
-                        {
-                            center: {
-                                latitude: market_latitude,
-                                longitude: market_longitude,
+        isBusy ? <Logo/> :
+            <View style={styles.container}>
+                <Logo/>
+                <MapView
+                ref={mapRef}
+                initialRegion={{
+                    latitude: mercados[0].market_latitude,
+                    longitude: mercados[0].market_longitude,
+                    latitudeDelta: 0.0142,
+                    longitudeDelta: 0.0131,
+                }}
+                style={styles.mapView}
+                rotateEnabled={false}
+                scrollEnabled={false}
+                zoomEnabled={false}
+                showsPointsOfInterest={false}
+                showsBuildings={false}
+                showsUserLocation={true}
+                showsMyLocationButton={true}
+                onMapReady={showFirstCallout}
+                >
+                    {mercados.map((mercado) => (
+                        <MapView.Marker
+                        ref={(markRef) => (mercado.markRef = markRef)}
+                        title={mercado.market_name}
+                        description={mercado.market_street + ', ' + mercado.market_number}
+                        key={mercado.market_id}
+                        coordinate={{
+                            latitude: mercado.market_latitude,
+                            longitude: mercado.market_longitude,
+                        }}
+                        >
+                            <Image
+                            source={require('../../../assets/img/marker.png')}
+                            style={styles.marker}
+                            />
+                        </MapView.Marker>
+                    ))}
+                </MapView>
+                
+                <ScrollView
+                style={styles.placesContainer}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                pagingEnabled
+                onMomentumScrollEnd={(e) => {
+                    const scrolled = e.nativeEvent.contentOffset.x
+    
+                    const place = scrolled > 0 ? scrolled / width : 0
+                
+                    console.log('prevPlace: ' + prevPlace)
+                    console.log('place: ' + place)
+    
+                    if (place != prevPlace) {
+                        const { market_latitude, market_longitude, markRef } = mercados[place]
+    
+                        mapRef.current.animateCamera(
+                            {
+                                center: {
+                                    latitude: market_latitude,
+                                    longitude: market_longitude,
+                                },
                             },
-                        },
-                        1000
-                    )
-                    
-                    markRef.showCallout()
-
-                    prevPlace = place
-                }
-            }}>
-                { mercados.map((mercado) => (
-                    <View key={mercado.market_id} style={styles.place}>
-                        <Text>{ mercado.market_name }</Text>
-                        <Text>{ mercado.market_street + ', ' + mercado.market_number }</Text>
-                    </View>
-                )) }
-            </ScrollView>
-                    
-            <TouchableOpacity 
-            style={styles.btnSugerir}
-            onPress={navigateToSuggest}
-            >
-                <Image
-                source={require('../../../assets/img/btnAddMarket.png')}
-                style={styles.btnSugerirImg}
-                />
-            </TouchableOpacity>
-        </View>
+                            10000
+                        )
+                        
+                        markRef.showCallout()
+    
+                        prevPlace = place
+                    } 
+                }}>
+                    {mercados.map((mercado) => (
+                        <View key={mercado.market_id} style={styles.place}>
+                            <Text>{ mercado.market_name }</Text>
+                            <Text>{ mercado.market_street + ', ' + mercado.market_number }</Text>
+                        </View>
+                    ))}
+                </ScrollView>
+                        
+                <TouchableOpacity 
+                style={styles.btnSugerir}
+                onPress={navigateToSuggest}
+                >
+                    <Image
+                    source={require('../../../assets/img/btnAddMarket.png')}
+                    style={styles.btnSugerirImg}
+                    />
+                </TouchableOpacity>
+            </View>
     )
 }
