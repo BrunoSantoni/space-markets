b1e9f1ea5f6f2ab5baae12a9c544702e0cf23f3c
pedrov4z <61953061+pedrov4z@users.noreply.github.com>
2020-05-25 05:46:15 -0300

    feat: MarketProducts estilizada

--- mobile/src/pages/market-products/index.js
+++ mobile/src/pages/market-products/index.js
@@ -3,83 +3,71 @@ import { View, Text, TouchableOpacity, Image } from 'react-native'
 
 import styles from './styles'
 
-import Icons from '@expo/vector-icons/FontAwesome'
-
 import { useNavigation } from '@react-navigation/native'
 
 import api from '../../services/api'
 
 export default function MarketProducts({ route }) {
-    const navigation = useNavigation()
-    const { marketId, marketName, marketPicture } = route.params
-    const [products, setProducts] = useState([])
+  const navigation = useNavigation()
+  const { marketId, marketName, marketPicture } = route.params
+  const [products, setProducts] = useState([])
 
-    useEffect(() => {
-        api.get('produtos', {
-            headers: {
-                auth: marketId
-            }
-        }).then(res => {
-            setProducts(res.data)
-        })
-    }, [marketId])
+  useEffect(() => {
+    api
+      .get('produtos', {
+        headers: {
+          auth: marketId,
+        },
+      })
+      .then((res) => {
+        setProducts(res.data)
+      })
+  }, [marketId])
 
-    function navigateToMap() {
-        navigation.goBack()
-    }
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: true,
+      headerTitle: () => <Text style={styles.headerName}>{marketName}</Text>,
+      headerRight: () => (
+        <Image source={{ uri: marketPicture }} style={styles.headerImg} />
+      ),
+    })
+  }, [products])
 
-    function navigateToSuggest() {
-        navigation.navigate('Suggest', {
-            marketId: marketId
-        })
-    }
+  function navigateToSuggest() {
+    navigation.navigate('Suggest', {
+      marketId: marketId,
+    })
+  }
 
-    const productsList = products.map((product, index) => (
-        index % 2 === 0 ?
-        (
-        <View style={[styles.product, styles.even]} key={product._id}>
-            <Image source={{uri: product.product_picture_url}} style={styles.productImg} />
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.suggestBtn}>
+        <Text style={styles.suggestBtnText} onPress={navigateToSuggest}>
+          Sugerir um produto
+        </Text>
+      </TouchableOpacity>
+      <View style={styles.productContainer}>
+        {products.map((product) => (
+          <View style={styles.product} key={product._id}>
+            <Image
+              source={{ uri: product.product_picture_url }}
+              style={styles.productImg}
+            />
             <View>
-                <Text style={styles.productName}>{product.product_name}</Text>
-                <View style={styles.priceAndName}>
-                    <Text style={styles.textPriceAndName}>R$ {product.product_price}</Text>
-                </View>
-                <Text style={styles.location}>Descrição: {product.product_description}</Text>
+              <Text style={styles.productName}>{product.product_name}</Text>
+              <View style={styles.priceAndName}>
+                <Text style={styles.textPriceAndName}>
+                  R$ {product.product_price}
+                </Text>
+              </View>
+              <Text style={styles.productDescription}>
+                {product.product_description}
+              </Text>
             </View>
-        </View>
-        ) : 
-        (
-        <View style={styles.product} key={product._id}>
-            <Image source={{uri: product.product_picture_url}} style={styles.productImg} />
-            <View>
-                <Text style={styles.productName}>{product.product_name}</Text>
-                <View style={styles.priceAndName}>
-                    <Text style={styles.textPriceAndName}>R$ {product.product_price}</Text>
-                </View>
-                <Text style={styles.location}>Descrição: {product.product_description}</Text>
-            </View>
-        </View>
-        )
-    ))
-
-    return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <Icons name="arrow-left" size={20} onPress={navigateToMap} style={styles.icon}/>
-                <Image source={{uri: marketPicture}} style={styles.headerImg} />                
-                <View>                    
-                    <Text style={styles.headerName}>{marketName}</Text>
-                    <TouchableOpacity style={styles.suggestBtn}>
-                        <Text style={styles.suggestBtnText} onPress={navigateToSuggest}>Sugerir Produto</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-            <View style={styles.sessionTitle}>
-                <Text style={styles.titleText}>Ofertas!</Text>
-            </View>
-            <View style={styles.productContainer}>
-                {productsList}
-            </View>
-        </View>
-    )
+          </View>
+        ))}
+      </View>
+    </View>
+  )
 }
--- mobile/src/pages/market-products/styles.js
+++ mobile/src/pages/market-products/styles.js
@@ -1,116 +1,80 @@
-import { StyleSheet, Dimensions } from 'react-native';
-import Constants from 'expo-constants';
-const { width } = Dimensions.get('window');
+import { StyleSheet, Dimensions } from 'react-native'
+const { width } = Dimensions.get('window')
 import Color from '../../constants/colors'
 
-export default StyleSheet.create({  
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        paddingTop: Constants.statusBarHeight
-    },
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
 
-    header: {
-        width: width,
-        backgroundColor: '#63b1b9',
-        height: 110,
-        flexDirection: 'row',
-        paddingHorizontal: 30,
-        alignItems: 'center'
-    },
+  headerImg: {
+    width: 40,
+    height: 40,
+    borderRadius: 30,
+    borderWidth: 1,
+    borderColor: '#000',
+    marginRight: 25,
+  },
 
-    buttons: {
-        flexDirection: 'row',
-        marginLeft: 5,
-        marginRight: 15,
-    },
+  headerName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
 
-    headerImg: {
-        width: 60,
-        height: 60,
-        borderRadius: 30,
-        marginRight: 30
-    },
+  suggestBtn: {
+    backgroundColor: '#63b1b9',
+    width: width / 2,
+    height: 40,
+    marginVertical: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8
+  },
 
-    headerName: {
-        fontSize: 25,
-        fontWeight: 'bold'
-    },
+  suggestBtnText: {
+    fontSize: 18,
+    color: '#fff'
+  },
 
-    suggestBtn: {
-        backgroundColor: Color.secundary,
-        width: 150,
-        height: 40,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 5,
-        borderWidth: 1,
-        borderColor: '#000',
-        marginTop: 5
-    },
+  productContainer: {
+    width: width - 15,
+  },
 
-    icon: {
-        position: 'absolute',
-        marginLeft: 5,
-    },
+  product: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 90,
+    paddingHorizontal: 20,
+    marginVertical: 10,
+    borderRadius: 10,
+    backgroundColor: '#D3D3D3'
+  },
 
-    suggestBtnText: {
-        fontSize: 18
-    },
+  productImg: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginRight: 20,
+    borderWidth: 1,
+    borderColor: '#000',
+  },
 
-    sessionTitle: {
-        width: width,
-        height: 37,
-        backgroundColor: Color.secundary,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
+  productName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
 
-    titleText: {
-        fontSize: 24,
-        textTransform: 'uppercase',
-        color: '#3A3A3A'
-    },
+  priceAndName: {
+    flexDirection: 'row',
+  },
 
-    productContainer: {
-        width: width,
-    },
+  textPriceAndName: {
+    fontSize: 16,
+    color: '#3A3A3A',
+  },
 
-    product: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        height: 90,
-        paddingHorizontal: 20,
-    },
-
-    even: {
-        backgroundColor: '#D3D3D3'
-    },
-    
-    productImg: {
-        width: 60,
-        height: 60,
-        borderRadius: 30,
-        marginRight: 20,
-        borderWidth: 1,
-        borderColor: '#000'
-    },
-
-    productName: {
-        fontSize: 20,
-        fontWeight: 'bold'
-    },
-
-    priceAndName: {
-        flexDirection: 'row'
-    },
-
-    textPriceAndName: {
-        fontSize: 16,
-        color: '#3A3A3A'
-    },
-
-    location: {
-        color: '#3A3A3A'
-    }
+  productDescription: {
+    color: '#3A3A3A',
+  },
 })
